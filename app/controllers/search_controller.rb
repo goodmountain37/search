0b4f92494f3f0411cf82a6d9679c47b77270f7e6
vagrant <goodmountain37@gmail.com>
2020-09-04 00:44:55 +0000

    [finish] search_function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,32 @@
 class SearchController < ApplicationController
+  def index
+    @search = params[:search]
+    @title_select = params[:model_select]
+
+    if params[:model_select] == "User"
+      if params[:search_select] == "完全一致"
+        @users = User.where('name LIKE ?', "#{params[:search]}")
+      elsif params[:search_select] == "前方一致"
+        @users = User.where('name LIKE ?', "#{params[:search]}%")
+      elsif params[:search_select] == "後方一致"
+        @users = User.where('name LIKE ?', "%#{params[:search]}")
+      elsif params[:search_select] == "部分一致"        
+        @users = User.where('name LIKE ?', "%#{params[:search]}%")
+      else
+        @users = User.all
+      end
+    elsif params[:model_select] == "Book"
+      if params[:search_select] == "完全一致"
+        @books = Book.where('title LIKE ?', "#{params[:search]}")
+      elsif params[:search_select] == "前方一致"
+        @books = Book.where('title LIKE ?', "#{params[:search]}%")
+      elsif params[:search_select] == "後方一致"
+        @books = Book.where('title LIKE ?', "%#{params[:search]}")
+      elsif params[:search_select] == "部分一致"        
+        @books = Book.where('title LIKE ?', "%#{params[:search]}%")
+      else
+        @books = Book.all
+      end
+    end
+  end
 end
